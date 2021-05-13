efaa78b012bd1988c59ee6262df937a7e9fdc768
Kleber <klebernascimento@outlook.com.br>
2021-05-13 15:55:23 -0300

    Finalizado a Aula 08

--- api-cities/src/main/java/com/cities/apicities/CountryResource.java
+++ api-cities/src/main/java/com/cities/apicities/CountryResource.java
@@ -4,11 +4,11 @@ import com.cities.apicities.countries.Country;
 import com.cities.apicities.repository.CountryRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/coutries")
@@ -23,6 +23,17 @@ public class CountryResource {
     @GetMapping
     public Page<Country> countries(Pageable page){
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id){
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
